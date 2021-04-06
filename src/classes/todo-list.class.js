a7290d3a1fbb05e5c67c8fe8851b3de18aaba814
soRodriguezz <s.rodriguezzapata01@gmail.com>
2021-04-06 14:28:21 -0400

    filters todo

--- src/classes/todo-list.class.js
+++ src/classes/todo-list.class.js
@@ -1,36 +1,44 @@
+import { Todo } from "./todo.class";
+
 export class TodoList {
   constructor() {
-    this.todos = [];
+    this.cargarLocalStorage();
   }
 
   nuevoTodo(todo) {
     this.todos.push(todo);
+    this.guardarLocalStorage();
   }
 
   eliminarTodo(id) {
     this.todos = this.todos.filter((todo) => todo.id != id);
+    this.guardarLocalStorage();
   }
 
   marcarCompletado(id) {
     for (const todo of this.todos) {
-      console.log(id, todo.id);
       if (todo.id == id) {
         todo.completado = !todo.completado;
+        this.cargarLocalStorage();
         break;
       }
     }
   }
 
   eliminarCompletados() {
-    this.todos = this.todos.filter( todo => !todo.completado);
+    this.todos = this.todos.filter((todo) => !todo.completado);
+    this.guardarLocalStorage();
   }
 
-  guardarLocalStorage(){
-    
+  guardarLocalStorage() {
+    localStorage.setItem("todo", JSON.stringify(this.todos));
   }
 
-  cargarLocalStorage(){
+  cargarLocalStorage() {
+    this.todos = localStorage.getItem("todo")
+      ? JSON.parse(localStorage.getItem("todo"))
+      : [];
 
+    this.todos = this.todos.map(obj => Todo.fromJson(obj));
   }
-
 }
--- src/index.js
+++ src/index.js
@@ -4,3 +4,6 @@ import { Todo, TodoList } from './classes'
 import { crearTodoHtml } from './js/componentes';
 
 export const todoList = new TodoList();
+
+todoList.todos.forEach(todo => crearTodoHtml(todo));
+
