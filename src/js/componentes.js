177e548bf790b6eba7ff71a0ba42414a370f6fac
soRodriguezz <s.rodriguezzapata01@gmail.com>
2021-04-06 14:28:04 -0400

    filter todo

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -6,6 +6,8 @@ import { todoList } from "../index";
 const divTodoList = document.querySelector(".todo-list");
 const txtInput = document.querySelector(".new-todo");
 const btnBorrar = document.querySelector(".clear-completed");
+const ulFiltros = document.querySelector(".filters");
+const anchorFiltros = document.querySelectorAll(".filtro");
 
 export const crearTodoHtml = (todo) => {
   const htmlTodo = `
@@ -60,4 +62,30 @@ btnBorrar.addEventListener('click', () => {
       divTodoList.removeChild(elemento);
     }
   }
+});
+
+ulFiltros.addEventListener('click', (event) => {
+  const filtro = event.target.text;
+  if(!filtro) return;
+
+  anchorFiltros.forEach(elem => elem.classList.remove('selected'));
+  event.target.classList.add('selected');
+
+  for(const elemento of divTodoList.children){
+    elemento.classList.remove('hidden');
+    const completado = elemento.classList.contains('completed');
+    switch(filtro){
+      case 'Pendientes':
+        if(completado){
+          elemento.classList.add('hidden');
+        }
+        break;
+      case 'Completados':
+        if(!completado){
+          elemento.classList.add('hidden');
+        }
+        break;
+    }
+  }
+
 });
